6fd6d2af617ea94e77ab8cddeb8f33092051bb25
Alexandr Afanasiev <oleksandr.afanasiev@masterofcode.com>
2021-04-05 19:35:53 +0300

    Improve handle and display connection errors

--- Package.swift
+++ Package.swift
@@ -12,20 +12,22 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "EmbeddableVoiceAssistant",
+            type: .dynamic,
             targets: ["EmbeddableVoiceAssistant"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(name: "Introspect", url: "https://github.com/siteline/SwiftUI-Introspect.git", from: "0.1.2"),
         .package(name: "AdaptiveCardUI", url: "https://github.com/Master-Of-Code-Global/AdaptiveCardUI", from: "0.1.0"),
-        .package(name: "DirectLine", url: "https://github.com/Master-Of-Code-Global/DirectLine", from: "0.1.0"),
+        .package(name: "DirectLine", url: "https://github.com/Master-Of-Code-Global/DirectLine", from: "0.1.1"),
+        .package(name: "Reachability", url: "https://github.com/ashleymills/Reachability.swift", from: "5.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "EmbeddableVoiceAssistant",
-            dependencies: ["Introspect", "AdaptiveCardUI", "DirectLine"],
+            dependencies: ["Introspect", "AdaptiveCardUI", "DirectLine", "Reachability"],
             resources: [
                   .copy("Roboto-Regular.ttf")
               ]),
